6933e2abd3075acf3691ed154eb15e8b541f357f
KAgaev <kenkosh@yandex.ru>
2021-04-20 09:38:07 +0300

    добавляет лайки ёжикам

--- source/js/modules/card-tooltip.js
+++ source/js/modules/card-tooltip.js
@@ -11,10 +11,13 @@ const cardTooltip = () => {
       this.button = this.tooltip.querySelector('.js-card-tooltip-button');
       this.content = this.tooltip.querySelector('.js-card-tooltip-content');
       this.activeClass = 'active';
+      this.likeButton = this.tooltip.querySelector('.js-like-btn');
+      this.likeCount = this.likeButton.querySelector('.js-like-count');
 
       this.toggleTooltip = this.toggleTooltip.bind(this);
 
       this.button.addEventListener('click', this.toggleTooltip);
+      this.likeButton.addEventListener('click', this.addLike.bind(this));
     }
 
     toggleTooltip(evt) {
@@ -22,6 +25,22 @@ const cardTooltip = () => {
 
       if (target) {
         this.tooltip.classList.toggle(this.activeClass);
+      }
+    }
+
+    addLike(evt) {
+      const target = evt.target.closest('.js-like-btn');
+
+      if (target) {
+        const likes = +this.likeCount.textContent;
+
+        if (!target.classList.contains(this.activeClass)) {
+          target.classList.add(this.activeClass);
+          this.likeCount.textContent = likes + 1;
+        } else {
+          target.classList.remove(this.activeClass);
+          this.likeCount.textContent = likes - 1;
+        }
       }
     }
   }
